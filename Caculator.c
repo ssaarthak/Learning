68e492e0591bb0dc63cc05774643298438510033
Saarthak Singh <saarthaksingh5663@gmail.com>
2024-11-23 21:05:22 +0530

    Updated Caculator.c
    Added continuous calculations

--- Caculator.c
+++ Caculator.c
@@ -1,47 +1,54 @@
 #include <stdio.h>
+# include <string.h>
 int main(){
-    //variable section
+// Variable & Arrays Section
 
     double num1, num2, rslt;
     char op;
+    char ch[4];
 
-    //input section
+// Input Section
     
     printf("Enter the first number: ");
     scanf("%lf", &num1);
     printf("\nEnter the second number: ");
     scanf("%lf", &num2);
-    printf("\nEnter the operation you want to performe: ");
-    scanf(" %c", &op);
-    
-    
-    //condition section
 
-    switch (op){
-    case'+':
-        rslt=(num1+num2);
-        printf("%.2lf", rslt);
-        break;
-    case'-':
-        rslt=(num1-num2);
-        printf("%.2lf", rslt);
-        break;
-    case'*':
-        rslt=(num1*num2);
-        printf("%.2lf", rslt);
-        break;
-    case'/':
-        if (num1!=0 && num2!=0){
-            rslt=(num1*num2);
-            printf("%.2lf", rslt);
+// Continuous Calculations & Condition Section
+
+
+    do{
+        printf("\nEnter the operation you want to performe: ");
+        scanf(" %c", &op);    
+        switch (op){
+            case'+':
+                rslt=(num1+num2);
+                printf("%.2lf", rslt);
+                break;
+            case'-':
+                rslt=(num1-num2);
+                printf("%.2lf", rslt);
+                break;
+            case'*':
+                rslt=(num1*num2);
+                printf("%.2lf", rslt);
+                break;
+            case'/':
+                if (num1!=0 && num2!=0){
+                    rslt=(num1*num2);
+                    printf("%.2lf", rslt);
+                }
+                else{
+                    printf("Invalid input you can't divide a number by zero.");
+                }
+                break;
+            default:
+                printf("\nYou have entered invalid input");
+                break;
         }
-        else{
-            printf("Invalid input you can't divide a number by zero.");
-        }
-        break;
-        default:
-        printf("\nYou have entered invalid input");
-        break;
+        printf("\n\nDo you want to perform more operations? (yes/Yes): ");
+        scanf("%s", &ch);
     }
+    while(strcmp(ch,"yes")==0 || strcmp(ch,"Yes")==0); 
     return 0;
 }
